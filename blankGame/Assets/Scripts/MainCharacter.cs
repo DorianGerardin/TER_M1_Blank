358859c673e82e645f145346efb2671a3bfa450e
DorianGerardin <gryfadon@gmail.com>
2022-03-21 23:54:38 +0100

    ajout animations mais pas fonctionnel

--- blankGame/Assets/Scripts/MainCharacter.cs
+++ blankGame/Assets/Scripts/MainCharacter.cs
@@ -8,13 +8,17 @@ public class MainCharacter : MonoBehaviour
     private Animator animator;
     private bool punchRight;
     private bool punchLeft;
+    private bool kickLeft;
+    private bool kickRight;
+    List<string> rightAnims = new List<string>();
+    List<string> leftAnims = new List<string>();
 
     private Rigidbody body;
     private Vector3 defaultColliderSize;
     private Vector3 defaultColliderCenter;
     private BoxCollider boxCollider;
 
-    private float dashSpeed = 12f;
+    private float dashSpeed = 25f;
     private float dashTime = 0.2f;
     private bool isDashing = false;
     private float dashStartTime = 0f;
@@ -29,6 +33,8 @@ public class MainCharacter : MonoBehaviour
         body = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
         animator = GetComponent<Animator>();
+        rightAnims = new List<string>();
+        leftAnims = new List<string>();
     }
 
     // Start is called before the first frame update
@@ -48,6 +54,13 @@ public class MainCharacter : MonoBehaviour
 
         bool punchRight = animator.GetBool("PunchRight");
         bool punchLeft = animator.GetBool("PunchLeft");
+        bool kickLeft = animator.GetBool("KickLeft");
+        bool kickRight = animator.GetBool("KickRight");
+
+        rightAnims.Add("PR");
+        rightAnims.Add("KR");
+        leftAnims.Add("PL");
+        leftAnims.Add("KL");
 
         defaultColliderCenter = boxCollider.center;
         defaultColliderSize = boxCollider.size;
@@ -65,6 +78,8 @@ public class MainCharacter : MonoBehaviour
             if(Time.time >= dashStartTime + dashTime){
                 animator.SetBool("PunchRight", false);
                 animator.SetBool("PunchLeft", false);
+                animator.SetBool("KickRight", false);
+                animator.SetBool("KickLeft", false);
                 isDashing = false;
                 body.velocity = Vector3.zero;
                 boxCollider.center = defaultColliderCenter;
@@ -93,12 +108,45 @@ public class MainCharacter : MonoBehaviour
         if(right) {
             boxCollider.center = new Vector3(boxCollider.center.x, boxCollider.center.y, 0.045f);
             boxCollider.size = new Vector3(boxCollider.size.x, boxCollider.size.y, 0.15f);
-            animator.SetBool("PunchRight", true);
+            setRandomRightAnimation();
         }
         else {
             boxCollider.center = new Vector3(boxCollider.center.x, boxCollider.center.y, 0.045f);
             boxCollider.size = new Vector3(boxCollider.size.x, boxCollider.size.y, 0.15f);
-            animator.SetBool("PunchLeft", true);
+            //setRandomLeftAnimation();
+            animator.SetBool("KickLeft", true);
+        }
+    }
+
+    private void setRandomRightAnimation() {
+        int randomIndex = Random.Range(0, rightAnims.Count);
+        Debug.Log(randomIndex);
+        string value = rightAnims[randomIndex];
+        switch(value) {
+            case "PR":
+                animator.SetBool("PunchRight", true);
+                break;
+            case "KR":
+                animator.SetBool("KickRight", true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void setRandomLeftAnimation() {
+        int randomIndex = Random.Range(0, leftAnims.Count);
+        Debug.Log(randomIndex);
+        string value = leftAnims[randomIndex];
+        switch(value) {
+            case "PL":
+                animator.SetBool("PunchLeft", true);
+                break;
+            case "KL":
+                animator.SetBool("KickLeft", true);
+                break;
+            default:
+                break;
         }
     }
 }
