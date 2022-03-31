38f30afa0d1c61c756bf6f748c055f63b13031f8
Thomas Scholl <thomas.scholl@etu.umontpellier.fr>
2022-04-01 00:00:03 +0200

    i forgor

--- blankGame/Assets/Scripts/GameManager.cs
+++ blankGame/Assets/Scripts/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //HP=3;       
+        HP=3;       
     }
 
     // Update is called once per frame
